bc335aada5a8681cafabf8d63607a887a7938151
valmsmith39a <valmsmith39a@gmail.com>
2016-01-26 23:54:24 -0800

    Update

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -16,7 +16,7 @@ function getItemsInStore(){
 			//console.log('in getItemsInStore() in main.js. data is:', data);
 			//debugger
 			arrayOfItemsObjectsG = data; 
-			//console.log('array of contacts objects array TYPE ', typeof arrayOfContactsObjectsG);
+			console.log('array of contacts objects array TYPE ', arrayOfItemsObjectsG);
 			updateArrayOfRowContainers();
 			displayRowContainers(); 
    });
@@ -30,17 +30,27 @@ function updateArrayOfRowContainers(){
 	$('.items-list').empty(); 
 	arrayOfRowContainersObjectsG.splice(0, arrayOfRowContainersObjectsG.length);
 
+	var $titleRow = $('<tr>').addClass('row row-container');
+	var $itemTitle = $('<td>').addClass('name-title col-md-6 col-xs-6').text('Item');
+	$titleRow.append($itemTitle);
+	var $priceTitle = $('<td>').addClass('price-title col-md-6 col-xs-6').text('Price');
+	$titleRow.append($priceTitle);
+	arrayOfRowContainersObjectsG.push($titleRow);
+
 	arrayOfItemsObjectsG.map(function(item){
 		var $rowContainer = $('<tr>').addClass('row row-container');
 		var $itemColumn = $('<td>').addClass('name-col col-md-6 col-xs-6').text(item.name); 
     $rowContainer.append($itemColumn); 
     var $priceColumn = $('<td>').addClass('price-col col-md-6 col-xs-6').text(item.price);
-		$rowContainer.append($priceColumn);		
+		$rowContainer.append($priceColumn);	
+		var $imageColumn = $('<td>').addClass('image-col col-md-6 col-xs-6').attr('img');
+		$rowContainer.append($priceColumn);			
     arrayOfRowContainersObjectsG.push($rowContainer);
 	});
 }
 
 function displayRowContainers(){
+	//$('.itemsInStore-list').append('<div>test</div>');
 	$('.itemsInStore-list').append(arrayOfRowContainersObjectsG);
 	//$('#items-input').val('');
 }
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+  /*
+  console.log('inside get index.html');
+  fs.readFile('./views/index.html', function(err, data){
+		var html = fs.readFileSync('./views/index.html').toString();
+		res.send(html);
+	});
+	*/
 });
 
 /* GET item for details page */
 router.get('/itemDetails', function(req, res, next) {
   res.render('itemDetails');
 });
-
-
 
 module.exports = router;
